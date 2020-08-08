dd18b73c8fe78b8729b5ac01d637745923dcfdb8
vagrant <vagrant@vagrant.vm>
2020-08-08 03:09:56 +0000

    added new banners and blank pages for all the site pages

--- openmech/pages/urls.py
+++ openmech/pages/urls.py
@@ -4,11 +4,12 @@ from . import views
 urlpatterns = [
     path('', views.home, name='pages-home'),
     path('dashboard/', views.dashboard, name='pages-dashboard'),
-    #path('blogs/', views.blogs, name='pages-blogs'),
-    #path('forum/', views.forum, name='pages-forum'),
-    #path('groups/', views.groups, name='pages-group'),
+    path('blogs/', views.blogs, name='pages-blogs'),
+    path('forum/', views.forum, name='pages-forum'),
+    path('groups/', views.groups, name='pages-groups'),
     path('learn/', views.learn, name='pages-learn'),
-    #path('mechlab/', views.mechlab, name='pages-mechlab'),
-    #path('ecosystem/', views.ecosystem, name='pages-ecosystem'),
-    #path('members/', views.members, name='pages-members'),
+    path('mechlab/', views.mechlab, name='pages-mechlab'),
+    path('ecosystem/', views.ecosystem, name='pages-ecosystem'),
+    path('members/', views.members, name='pages-members'),
+    path('about/', views.about, name='pages-about'),
 ]
--- openmech/pages/views.py
+++ openmech/pages/views.py
@@ -9,7 +9,23 @@ def dashboard (request):
 def learn (request):
     return render (request, 'pages/learn.html')
 
+def about (request):
+    return render (request, 'pages/about.html')
 
+def blogs (request):
+    return render (request, 'pages/blogs.html')
 
+def forum (request):
+    return render (request, 'pages/forum.html')
 
+def mechlab (request):
+    return render (request, 'pages/mechlab.html')
 
+def ecosystem (request):
+    return render (request, 'pages/ecosytem.html')
+
+def groups (request):
+    return render (request, 'pages/groups.html')
+
+def members (request):
+    return render (request, 'pages/members.html')
